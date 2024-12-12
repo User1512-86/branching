8c5c1f59d57f18305c9b1f434a751453f8628553
Илья <ilyar151202@gmail.com>
2024-12-12 18:24:56 +0300

    1й доп коммит в dev

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 class Main{
     public static void main(String[] args) {
 
-        System.out.print("Это добавлено в ветке dev");
+        System.out.println("Это добавлено в ветке dev");
+        System.out.println("Это коммит №", 1);
 
     }
 }
