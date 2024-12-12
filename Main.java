0fb9cbb11a8550ce9301e05fcc1ae39742d8835b
Илья <ilyar151202@gmail.com>
2024-12-12 17:41:58 +0300

    Это 2й доп коммит

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
         System.out.println("Это строчка для +1 коммита");
+        System.out.println("Это строчка для +2 коммита");
     }
 }
