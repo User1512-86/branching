c6c99303952915dda80981bd49c6c96268e9bfe4
Илья <ilyar151202@gmail.com>
2024-12-12 19:28:20 +0300

    Пуш ветки main с изменениями

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ class Main{
         System.out.println("Это строчка для +2 коммита");
         System.out.println("Это строчка для +3 коммита");
         System.out.println("Это строчка для +4 коммита");
+        System.out.println("Изменение в main перед пушем");
     }
 }
